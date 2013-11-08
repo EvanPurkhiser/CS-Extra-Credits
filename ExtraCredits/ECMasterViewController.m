2d26e1f5df536ecf53a1153bd8dac7591f9331a2
Matthew Watzman <mwatz122@gmail.com>
2013-11-08 17:52:21 -0500

    Added NSLog

--- ExtraCredits/ECMasterViewController.m
+++ ExtraCredits/ECMasterViewController.m
@@ -34,6 +34,8 @@
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
     self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (ECDetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
+    
+    NSLog(@"Hello universe.");
 }
 
 - (void)didReceiveMemoryWarning
