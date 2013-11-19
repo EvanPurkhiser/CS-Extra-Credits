f89f68b53743b5040ecb9c934c4b3c3816e95aab
Drew Guarnera <dtg3@zips.uakron.edu>
2013-11-18 23:56:31 -0500

    Disabled Master View Edit and Inset Buttons
    Not sure if we need them since classes will be loaded via remote Plist or internal Plist.
    Code is just commented out for now.

--- ExtraCredits/ECMasterViewController.m
+++ ExtraCredits/ECMasterViewController.m
@@ -34,10 +34,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+    //self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
+    //UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
+    //self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (ECDetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
     
     // Get paths from the root directory
