a409a76126044a2f083ef440429166a5a83bf068
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-11-19 19:33:28 -0500

    Sort by subject:number

--- ExtraCredits/ECMasterViewController.m
+++ ExtraCredits/ECMasterViewController.m
@@ -154,9 +154,9 @@
     // Set the batch size to a suitable number.
     [fetchRequest setFetchBatchSize:20];
     
-    // Edit the sort key as appropriate.
-    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];
-    NSArray *sortDescriptors = @[sortDescriptor];
+    NSSortDescriptor *sortSubject = [[NSSortDescriptor alloc] initWithKey:@"subject" ascending:YES];
+    NSSortDescriptor *sortNumber  = [[NSSortDescriptor alloc] initWithKey:@"number"  ascending:YES];
+    NSArray *sortDescriptors = @[sortSubject, sortNumber];
     
     [fetchRequest setSortDescriptors:sortDescriptors];
     
