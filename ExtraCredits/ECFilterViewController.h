b12d49cdb7a593a76c7befe6b6ea7215614bcf20
Drew Guarnera <dtg3@zips.uakron.edu>
2013-11-21 17:38:57 -0500

    Work on Filter Modal Begins
    Told you there would be micro commits.

---
+++ ExtraCredits/ECFilterViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECFilterViewController.h
+//  ExtraCredits
+//
+//  Created by Drew Guarnera on 11/21/13.
+//  Copyright (c) 2013 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECFilterViewController : UITableViewController
+
+@end
