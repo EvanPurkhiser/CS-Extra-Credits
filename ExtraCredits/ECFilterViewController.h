1c7f4a0e5829539dc841f79a19abfb69d6da315e
Drew Guarnera <dtg3@zips.uakron.edu>
2013-11-21 18:10:05 -0500

    Setup Plumbing For FilterViewController

---
+++ ExtraCredits/ECFilterViewController.h
@@ -0,0 +1,25 @@
+//
+//  ECFilterViewController.h
+//  ExtraCredits
+//
+//  Created by Drew Guarnera on 11/21/13.
+//  Copyright (c) 2013 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ECFilterViewController;
+
+@protocol ECFilterViewControllerDelegate <NSObject>
+- (void)filterViewControllerDidCancel:(ECFilterViewController *)controller;
+- (void)filterViewControllerDidSave:(ECFilterViewController *)controller;
+@end
+
+@interface ECFilterViewController : UITableViewController
+
+@property (nonatomic, weak) id <ECFilterViewControllerDelegate> delegate;
+
+- (IBAction)cancel:(id)sender;
+- (IBAction)done:(id)sender;
+
+@end
