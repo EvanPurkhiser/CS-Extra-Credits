8973fbd364518631e044c6eb96b5f1f643804d23
Matthew Watzman <mwatz122@gmail.com>
2013-11-21 18:28:55 -0500

    Added view controllers for advising and settings tabs, linked them with storyboard.

---
+++ ExtraCredits/ECAdvisingViewController.m
@@ -0,0 +1,38 @@
+//
+//  ECAdvisingViewController.m
+//  ExtraCredits
+//
+//  Created by Drew Guarnera, Matthew Watzman, Evan Purkhiser on 11/8/13.
+//  Copyright (c) 2013 iOS. All rights reserved.
+//
+
+#import "ECAdvisingViewController.h"
+
+@interface ECAdvisingViewController ()
+
+@end
+
+@implementation ECAdvisingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ExtraCredits/ECSettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ECSettingsViewController.m
+//  ExtraCredits
+//
+//  Created by Drew Guarnera, Matthew Watzman, Evan Purkhiser on 11/8/13.
+//  Copyright (c) 2013 iOS. All rights reserved.
+//
+
+#import "ECSettingsViewController.h"
+
+@interface ECSettingsViewController ()
+
+@end
+
+@implementation ECSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
