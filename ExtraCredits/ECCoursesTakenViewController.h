262bb4f729ed3efe453fd059b0b3fe16e143f3de
Drew Guarnera <dtg3@zips.uakron.edu>
2013-11-29 21:05:51 -0500

    Support Setting Courses Taken

---
+++ ExtraCredits/ECCoursesTakenViewController.h
@@ -0,0 +1,13 @@
+//
+//  ECCoursesTakenViewController.h
+//  ExtraCredits
+//
+//  Created by Drew Guarnera on 11/29/13.
+//  Copyright (c) 2013 iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ECCoursesTakenViewController : UITableViewController
+
+@end
--- ExtraCredits/ECSettingsViewController.h
+++ ExtraCredits/ECSettingsViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ECSettingsViewController : UIViewController
+@interface ECSettingsViewController : UITableViewController
 
 @end
